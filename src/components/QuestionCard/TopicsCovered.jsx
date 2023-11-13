3ab3e360783e2f4672d32038c0e0fb6913eb5071
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-13 15:59:47 +0530

    PropTypes

--- src/components/QuestionCard/TopicsCovered.jsx
+++ src/components/QuestionCard/TopicsCovered.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import PropTypes from "prop-types";
 
 const TopicsCovered = ({ title }) => {
     return <span className="px-3 m-1 rounded-full bg-white/30">{title}</span>;
+};
+
+TopicsCovered.propTypes = {
+    title: PropTypes.string,
 };
 
 export default TopicsCovered;
