b11c8ff1abddfecda19ff1ae9ed24572b419fdb4
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-14 20:20:42 +0530

    styled div added

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -21,8 +21,8 @@ const SearchBar = ({ questionData, setFilteredQuestionData }) => {
     }, [searchText]);
 
     return (
-        <div className="p-4 flex justify-center items-center gap-2">
-            <label htmlFor="search-bar">Search Bar</label>
+        <div className="p-4 w-full flex justify-center items-center gap-2">
+            {/* <label htmlFor="search-bar">Search Bar</label> */}
             <input
                 value={searchText}
                 onChange={(e) => {
@@ -30,7 +30,8 @@ const SearchBar = ({ questionData, setFilteredQuestionData }) => {
                 }}
                 id="search-bar"
                 type="search"
-                className=" "
+                placeholder="Search for a question..."
+                className="px-4 py-2 w-1/5 shadow-lg rounded-md focus:ring-1 outline-none focus:ring-orange-800 focus:ring-inset "
             />
         </div>
     );
